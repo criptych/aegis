2af506255625db763c55dbc65e4b979b23f180e4
criptych <criptych@gmail.com>
2015-07-15 11:37:45 -0400

    Add aeExtent operations

--- src/aegeom.hpp
+++ src/aegeom.hpp
@@ -31,6 +31,56 @@ struct aeExtentT {
     ): min(min), max(max) {
     }
 
+    aeExtentT<T> &operator |= (const aePointT<T> &rhs) {
+        if (min.x > rhs.x || std::isnan(min.x)) { min.x = rhs.x; }
+        if (min.y > rhs.y || std::isnan(min.y)) { min.y = rhs.y; }
+        if (min.z > rhs.z || std::isnan(min.z)) { min.z = rhs.z; }
+        if (min.m > rhs.m || std::isnan(min.m)) { min.m = rhs.m; }
+        if (max.x < rhs.x || std::isnan(max.x)) { max.x = rhs.x; }
+        if (max.y < rhs.y || std::isnan(max.y)) { max.y = rhs.y; }
+        if (max.z < rhs.z || std::isnan(max.z)) { max.z = rhs.z; }
+        if (max.m < rhs.m || std::isnan(max.m)) { max.m = rhs.m; }
+        return *this;
+    }
+
+    aeExtentT<T> &operator |= (const aeExtentT<T> &rhs) {
+        if (min.x > rhs.min.x || std::isnan(min.x)) { min.x = rhs.min.x; }
+        if (min.y > rhs.min.y || std::isnan(min.y)) { min.y = rhs.min.y; }
+        if (min.z > rhs.min.z || std::isnan(min.z)) { min.z = rhs.min.z; }
+        if (min.m > rhs.min.m || std::isnan(min.m)) { min.m = rhs.min.m; }
+        if (max.x < rhs.max.x || std::isnan(max.x)) { max.x = rhs.max.x; }
+        if (max.y < rhs.max.y || std::isnan(max.y)) { max.y = rhs.max.y; }
+        if (max.z < rhs.max.z || std::isnan(max.z)) { max.z = rhs.max.z; }
+        if (max.m < rhs.max.m || std::isnan(max.m)) { max.m = rhs.max.m; }
+        return *this;
+    }
+
+    aeExtentT<T> &operator &= (const aeExtentT<T> &rhs) {
+        if (min.x < rhs.min.x || std::isnan(min.x)) { min.x = rhs.min.x; }
+        if (min.y < rhs.min.y || std::isnan(min.y)) { min.y = rhs.min.y; }
+        if (min.z < rhs.min.z || std::isnan(min.z)) { min.z = rhs.min.z; }
+        if (min.m < rhs.min.m || std::isnan(min.m)) { min.m = rhs.min.m; }
+        if (max.x > rhs.max.x || std::isnan(max.x)) { max.x = rhs.max.x; }
+        if (max.y > rhs.max.y || std::isnan(max.y)) { max.y = rhs.max.y; }
+        if (max.z > rhs.max.z || std::isnan(max.z)) { max.z = rhs.max.z; }
+        if (max.m > rhs.max.m || std::isnan(max.m)) { max.m = rhs.max.m; }
+        return *this;
+    }
+
+    template <typename X>
+    aeExtentT<T> operator | (const X &rhs) const {
+        aeExtentT<T> result(*this);
+        result |= rhs;
+        return result;
+    }
+
+    template <typename X>
+    aeExtentT<T> operator & (const X &rhs) const {
+        aeExtentT<T> result(*this);
+        result &= rhs;
+        return result;
+    }
+
     bool isEmpty() const {
         return (min.x == max.x && min.y == max.y) ||
                 std::isnan(min.x) || std::isnan(min.y) ||
--- src/tests.cpp
+++ src/tests.cpp
@@ -7,6 +7,36 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+TEST_CASE("extent operations", "[extent]") {
+    aeExtent e1 = { aePoint(0.0, 0.0), aePoint(1.0, 1.0) };
+    aeExtent e2 = { aePoint(1.0, 1.0), aePoint(2.0, 2.0) };
+
+    SECTION("check attributes") {
+        CHECK(!e1.isEmpty());
+        CHECK(!e2.isEmpty());
+    }
+
+    SECTION("check union") {
+        aeExtent e = e1 | e2;
+        CHECK(!e.isEmpty());
+        CHECK(e.min.x == Approx(0.0));
+        CHECK(e.min.y == Approx(0.0));
+        CHECK(e.max.x == Approx(2.0));
+        CHECK(e.max.y == Approx(2.0));
+    }
+
+    SECTION("check intersection") {
+        aeExtent e = e1 & e2;
+        CHECK(e.isEmpty());
+        CHECK(e.min.x == Approx(1.0));
+        CHECK(e.min.y == Approx(1.0));
+        CHECK(e.max.x == Approx(1.0));
+        CHECK(e.max.y == Approx(1.0));
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 TEST_CASE("polygon area", "[geometry][area]") {
     aeGeometry g = { aeGeometry::Polygon };
 
