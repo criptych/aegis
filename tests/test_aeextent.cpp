04e491daa3fb835e4cd6a5fcccb6479ccc6796af
criptych <criptych@gmail.com>
2015-07-15 11:55:33 -0400

    Split tests by class and move to own folder

---
+++ tests/test_aeextent.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+#include "aeextent.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("extent operations", "[aeExtent][extent]") {
+    aeExtent e1 = { aePoint(0.0, 0.0), aePoint(1.0, 1.0) };
+    aeExtent e2 = { aePoint(1.0, 1.0), aePoint(2.0, 2.0) };
+
+    SECTION("check attributes") {
+        CHECK(!e1.isEmpty());
+        CHECK(!e2.isEmpty());
+    }
+
+    SECTION("check union") {
+        aeExtent e = e1 | e2;
+        CHECK(!e.isEmpty());
+        CHECK(e.min.x == Approx(0.0));
+        CHECK(e.min.y == Approx(0.0));
+        CHECK(e.max.x == Approx(2.0));
+        CHECK(e.max.y == Approx(2.0));
+    }
+
+    SECTION("check intersection") {
+        aeExtent e = e1 & e2;
+        CHECK(e.isEmpty());
+        CHECK(e.min.x == Approx(1.0));
+        CHECK(e.min.y == Approx(1.0));
+        CHECK(e.max.x == Approx(1.0));
+        CHECK(e.max.y == Approx(1.0));
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//  EOF
+////////////////////////////////////////////////////////////////////////////////
+
--- tests/test_aegeom.cpp
+++ tests/test_aegeom.cpp
@@ -2,38 +2,8 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "aegis.hpp"
 #include "catch.hpp"
-
-////////////////////////////////////////////////////////////////////////////////
-
-TEST_CASE("extent operations", "[aeExtent][extent]") {
-    aeExtent e1 = { aePoint(0.0, 0.0), aePoint(1.0, 1.0) };
-    aeExtent e2 = { aePoint(1.0, 1.0), aePoint(2.0, 2.0) };
-
-    SECTION("check attributes") {
-        CHECK(!e1.isEmpty());
-        CHECK(!e2.isEmpty());
-    }
-
-    SECTION("check union") {
-        aeExtent e = e1 | e2;
-        CHECK(!e.isEmpty());
-        CHECK(e.min.x == Approx(0.0));
-        CHECK(e.min.y == Approx(0.0));
-        CHECK(e.max.x == Approx(2.0));
-        CHECK(e.max.y == Approx(2.0));
-    }
-
-    SECTION("check intersection") {
-        aeExtent e = e1 & e2;
-        CHECK(e.isEmpty());
-        CHECK(e.min.x == Approx(1.0));
-        CHECK(e.min.y == Approx(1.0));
-        CHECK(e.max.x == Approx(1.0));
-        CHECK(e.max.y == Approx(1.0));
-    }
-}
+#include "aegeom.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -205,111 +175,6 @@ TEST_CASE("simple polygons", "[aeGeometry][simple]") {
 
         REQUIRE(g.points().size() == 4);
         CHECK(g.findIntersections());
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-TEST_CASE("statistics calculation", "[aeStats]") {
-    static const double EPSILON = 0.0000000001;
-
-    SECTION("empty data set") {
-        aeStats st;
-
-        REQUIRE(st.count() == 0);
-        CHECK(std::isnan(st.mean()));
-        CHECK(std::isnan(st.variance(true)));
-        CHECK(std::isnan(st.stdev(true)));
-        CHECK(std::isnan(st.variance(false)));
-        CHECK(std::isnan(st.stdev(false)));
-        CHECK(std::isnan(st.skewness()));
-        CHECK(std::isnan(st.kurtosis()));
-    }
-
-    SECTION("single value [1]") {
-        aeStats st;
-        st.update(1.0);
-
-        REQUIRE(st.count() == 1);
-        CHECK(st.mean() == Approx(1.0).epsilon(EPSILON));
-        CHECK(std::isnan(st.variance(true)));
-        CHECK(std::isnan(st.stdev(true)));
-        CHECK(std::isnan(st.variance(false)));
-        CHECK(std::isnan(st.stdev(false)));
-        CHECK(std::isnan(st.skewness()));
-        CHECK(std::isnan(st.kurtosis()));
-    }
-
-    SECTION("two values [1 2]") {
-        aeStats st;
-        st.update(1.0);
-        st.update(2.0);
-
-        REQUIRE(st.count() == 2);
-        CHECK(st.mean() == Approx(1.5).epsilon(EPSILON));
-        CHECK(st.variance(true) == Approx(0.25).epsilon(EPSILON));
-        CHECK(st.stdev(true) == Approx(0.5).epsilon(EPSILON));
-        CHECK(st.variance(false) == Approx(0.5).epsilon(EPSILON));
-        CHECK(st.stdev(false) == Approx(0.7071067812).epsilon(EPSILON));
-        CHECK(st.skewness() == Approx(0.0).epsilon(EPSILON));
-        CHECK(st.kurtosis() == Approx(-2.0).epsilon(EPSILON));
-    }
-
-    SECTION("sample data: [2, 4, 4, 4, 5, 5, 7, 9]") {
-        double data[] = { 2, 4, 4, 4, 5, 5, 7, 9 };
-        aeStats st(std::begin(data), std::end(data));
-
-        REQUIRE(st.count() == 8);
-        CHECK(st.mean() == Approx(5.0).epsilon(EPSILON));
-        CHECK(st.variance(true) == Approx(4.0).epsilon(EPSILON));
-        CHECK(st.stdev(true) == Approx(2.0).epsilon(EPSILON));
-        CHECK(st.variance(false) == Approx(4.5714285714).epsilon(EPSILON));
-        CHECK(st.stdev(false) == Approx(2.1380899352).epsilon(EPSILON));
-        CHECK(st.skewness() == Approx(0.656250).epsilon(EPSILON));
-        CHECK(st.kurtosis() == Approx(-0.218750).epsilon(EPSILON));
-    }
-
-    SECTION("sample data: [2, 4, 4, 4] and [5, 5, 7, 9]") {
-        double data1[] = { 2, 4, 4, 4 };
-        double data2[] = { 5, 5, 7, 9 };
-
-        aeStats st1(std::begin(data1), std::end(data1));
-        aeStats st2(std::begin(data2), std::end(data2));
-
-        SECTION("list 1 statistics") {
-            REQUIRE(st1.count() == 4);
-            CHECK(st1.mean() == Approx(3.5).epsilon(EPSILON));
-            CHECK(st1.variance(true) == Approx(0.75).epsilon(EPSILON));
-            CHECK(st1.stdev(true) == Approx(0.8660254038).epsilon(EPSILON));
-            CHECK(st1.variance(false) == Approx(1.0).epsilon(EPSILON));
-            CHECK(st1.stdev(false) == Approx(1.0).epsilon(EPSILON));
-            CHECK(st1.skewness() == Approx(-1.1547005384).epsilon(EPSILON));
-            CHECK(st1.kurtosis() == Approx(-0.6666666667).epsilon(EPSILON));
-        }
-
-        SECTION("list 2 statistics") {
-            REQUIRE(st2.count() == 4);
-            CHECK(st2.mean() == Approx(6.5).epsilon(EPSILON));
-            CHECK(st2.variance(true) == Approx(2.75).epsilon(EPSILON));
-            CHECK(st2.stdev(true) == Approx(1.6583123952).epsilon(EPSILON));
-            CHECK(st2.variance(false) == Approx(3.6666666667).epsilon(EPSILON));
-            CHECK(st2.stdev(false) == Approx(1.9148542155).epsilon(EPSILON));
-            CHECK(st2.skewness() == Approx(0.4933822002).epsilon(EPSILON));
-            CHECK(st2.kurtosis() == Approx(-1.3719008264).epsilon(EPSILON));
-        }
-
-        aeStats st(st1 + st2);
-
-        SECTION("combined statistics") {
-            REQUIRE(st.count() == 8);
-            CHECK(st.mean() == Approx(5.0).epsilon(EPSILON));
-            CHECK(st.variance(true) == Approx(4.0).epsilon(EPSILON));
-            CHECK(st.stdev(true) == Approx(2.0).epsilon(EPSILON));
-            CHECK(st.variance(false) == Approx(4.5714285714).epsilon(EPSILON));
-            CHECK(st.stdev(false) == Approx(2.1380899352).epsilon(EPSILON));
-            CHECK(st.skewness() == Approx(0.656250).epsilon(EPSILON));
-            CHECK(st.kurtosis() == Approx(-0.218750).epsilon(EPSILON));
-        }
     }
 }
 
