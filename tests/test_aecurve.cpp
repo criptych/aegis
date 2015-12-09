935ad158141f7b1e65846f4214c3fb351d40771e
criptych <criptych@gmail.com>
2015-12-09 16:20:57 -0500

    Add more parentheses, silence more warnings
    Need a better fix for this (see discussion at philsquared/catch#528).

--- tests/test_aecurve.cpp
+++ tests/test_aecurve.cpp
@@ -24,7 +24,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
     aeCubicBezierCurve cubic;
 
     SECTION("empty curve") {
-        REQUIRE(cubic.controlPoints().size() == 0);
+        REQUIRE((cubic.controlPoints().size() == 0));
         CHECK_THROWS_AS(cubic.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(cubic[0.5], aeInvalidStateError);
     }
@@ -34,7 +34,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         quad.controlPoints().push_back(aePoint(0.5, 1.0));
         quad.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE(quad.controlPoints().size() == 3);
+        REQUIRE((quad.controlPoints().size() == 3));
 
         GIVEN("x == 0.25") {
             aePoint p(quad.evaluate(0.25));
@@ -60,7 +60,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(0.5, 1.0));
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE(cubic.controlPoints().size() == 3);
+        REQUIRE((cubic.controlPoints().size() == 3));
         CHECK_THROWS_AS(cubic.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(cubic[0.5], aeInvalidStateError);
     }
@@ -71,7 +71,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(1.0, 1.0));
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE(cubic.controlPoints().size() == 4);
+        REQUIRE((cubic.controlPoints().size() == 4));
 
         GIVEN("x == 0.25") {
             aePoint p(cubic.evaluate(0.25));
@@ -98,7 +98,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
         cubic.controlPoints().push_back(aePoint(1.0, 1.0));
 
-        REQUIRE(cubic.controlPoints().size() == 4);
+        REQUIRE((cubic.controlPoints().size() == 4));
 
         GIVEN("x == 0.25") {
             aePoint p(cubic.evaluate(0.25));
@@ -124,7 +124,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
     aeCubicNurbsCurve curve;
 
     SECTION("empty curve") {
-        REQUIRE(curve.controlPoints().size() == 0);
+        REQUIRE((curve.controlPoints().size() == 0));
         CHECK_THROWS_AS(curve.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(curve[0.5], aeInvalidStateError);
     }
@@ -134,7 +134,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(0.5, 1.0));
         curve.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE(curve.controlPoints().size() == 3);
+        REQUIRE((curve.controlPoints().size() == 3));
         CHECK_THROWS_AS(curve.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(curve[0.5], aeInvalidStateError);
     }
@@ -146,7 +146,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(1.0, 0.0));
 
         aePoint p;
-        REQUIRE(curve.controlPoints().size() == 4);
+        REQUIRE((curve.controlPoints().size() == 4));
         REQUIRE_NOTHROW(p = curve.evaluate(0.5));
         CHECK((p.x == Approx(0.5)));
         CHECK((p.y == Approx(0.75)));
@@ -159,7 +159,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(1.0, 1.0));
 
         aePoint p;
-        REQUIRE(curve.controlPoints().size() == 4);
+        REQUIRE((curve.controlPoints().size() == 4));
         REQUIRE_NOTHROW(p = curve.evaluate(0.5));
         CHECK((p.x == Approx(0.5)));
         CHECK((p.y == Approx(0.5)));
