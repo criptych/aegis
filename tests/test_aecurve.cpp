78dc4e1de78a026866f7909edaaf55571e4e4a98
criptych <criptych@gmail.com>
2015-12-10 10:34:19 -0500

    Fix(?) using latest GCC on Travis (and revert "missing" parentheses)

--- tests/test_aecurve.cpp
+++ tests/test_aecurve.cpp
@@ -24,7 +24,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
     aeCubicBezierCurve cubic;
 
     SECTION("empty curve") {
-        REQUIRE((cubic.controlPoints().size() == 0));
+        REQUIRE(cubic.controlPoints().size() == 0);
         CHECK_THROWS_AS(cubic.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(cubic[0.5], aeInvalidStateError);
     }
@@ -34,24 +34,24 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         quad.controlPoints().push_back(aePoint(0.5, 1.0));
         quad.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE((quad.controlPoints().size() == 3));
+        REQUIRE(quad.controlPoints().size() == 3);
 
         GIVEN("x == 0.25") {
             aePoint p(quad.evaluate(0.25));
-            CHECK((p.x == Approx(0.250).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.375).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.250).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.375).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.5") {
             aePoint p(quad.evaluate(0.50));
-            CHECK((p.x == Approx(0.500).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.500).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.500).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.500).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.75") {
             aePoint p(quad.evaluate(0.75));
-            CHECK((p.x == Approx(0.750).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.375).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.750).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.375).epsilon(EPSILON));
         }
     }
 
@@ -60,7 +60,7 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(0.5, 1.0));
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE((cubic.controlPoints().size() == 3));
+        REQUIRE(cubic.controlPoints().size() == 3);
         CHECK_THROWS_AS(cubic.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(cubic[0.5], aeInvalidStateError);
     }
@@ -71,24 +71,24 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(1.0, 1.0));
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE((cubic.controlPoints().size() == 4));
+        REQUIRE(cubic.controlPoints().size() == 4);
 
         GIVEN("x == 0.25") {
             aePoint p(cubic.evaluate(0.25));
-            CHECK((p.x == Approx(0.15625).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.56250).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.15625).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.56250).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.5") {
             aePoint p(cubic.evaluate(0.5));
-            CHECK((p.x == Approx(0.50000).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.75000).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.50000).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.75000).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.75") {
             aePoint p(cubic.evaluate(0.75));
-            CHECK((p.x == Approx(0.84375).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.56250).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.84375).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.56250).epsilon(EPSILON));
         }
     }
 
@@ -98,24 +98,24 @@ TEST_CASE("bezier curve evaluation", "[aeCurve]") {
         cubic.controlPoints().push_back(aePoint(1.0, 0.0));
         cubic.controlPoints().push_back(aePoint(1.0, 1.0));
 
-        REQUIRE((cubic.controlPoints().size() == 4));
+        REQUIRE(cubic.controlPoints().size() == 4);
 
         GIVEN("x == 0.25") {
             aePoint p(cubic.evaluate(0.25));
-            CHECK((p.x == Approx(0.15625).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.43750).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.15625).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.43750).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.5") {
             aePoint p(cubic.evaluate(0.5));
-            CHECK((p.x == Approx(0.50000).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.75000).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.50000).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.75000).epsilon(EPSILON));
         }
 
         GIVEN("x == 0.75") {
             aePoint p(cubic.evaluate(0.75));
-            CHECK((p.x == Approx(0.84375).epsilon(EPSILON)));
-            CHECK((p.y == Approx(0.56250).epsilon(EPSILON)));
+            CHECK(p.x == Approx(0.84375).epsilon(EPSILON));
+            CHECK(p.y == Approx(0.56250).epsilon(EPSILON));
         }
     }
 }
@@ -124,7 +124,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
     aeCubicNurbsCurve curve;
 
     SECTION("empty curve") {
-        REQUIRE((curve.controlPoints().size() == 0));
+        REQUIRE(curve.controlPoints().size() == 0);
         CHECK_THROWS_AS(curve.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(curve[0.5], aeInvalidStateError);
     }
@@ -134,7 +134,7 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(0.5, 1.0));
         curve.controlPoints().push_back(aePoint(1.0, 0.0));
 
-        REQUIRE((curve.controlPoints().size() == 3));
+        REQUIRE(curve.controlPoints().size() == 3);
         CHECK_THROWS_AS(curve.evaluate(0.5), aeInvalidStateError);
         CHECK_THROWS_AS(curve[0.5], aeInvalidStateError);
     }
@@ -146,10 +146,10 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(1.0, 0.0));
 
         aePoint p;
-        REQUIRE((curve.controlPoints().size() == 4));
+        REQUIRE(curve.controlPoints().size() == 4);
         REQUIRE_NOTHROW(p = curve.evaluate(0.5));
-        CHECK((p.x == Approx(0.5)));
-        CHECK((p.y == Approx(0.75)));
+        CHECK(p.x == Approx(0.5));
+        CHECK(p.y == Approx(0.75));
     }
 
     SECTION("simple S-curve") {
@@ -159,10 +159,10 @@ TEST_CASE("NURBS curve evaluation", "[aeCurve]") {
         curve.controlPoints().push_back(aePoint(1.0, 1.0));
 
         aePoint p;
-        REQUIRE((curve.controlPoints().size() == 4));
+        REQUIRE(curve.controlPoints().size() == 4);
         REQUIRE_NOTHROW(p = curve.evaluate(0.5));
-        CHECK((p.x == Approx(0.5)));
-        CHECK((p.y == Approx(0.5)));
+        CHECK(p.x == Approx(0.5));
+        CHECK(p.y == Approx(0.5));
     }
 }
 
--- tests/test_aeextent.cpp
+++ tests/test_aeextent.cpp
@@ -12,36 +12,36 @@ TEST_CASE("extent operations", "[aeExtent][extent]") {
     aeExtent e2 = { aePoint(1.0, 1.0), aePoint(2.0, 2.0) };
 
     SECTION("check attributes") {
-        CHECK((!e1.isEmpty()));
-        CHECK((!e2.isEmpty()));
+        CHECK(!e1.isEmpty());
+        CHECK(!e2.isEmpty());
     }
 
     SECTION("check union") {
         aeExtent e = e1 | e2;
-        CHECK((!e.isEmpty()));
-        CHECK((e.min.x == Approx(0.0)));
-        CHECK((e.min.y == Approx(0.0)));
-        CHECK((e.max.x == Approx(2.0)));
-        CHECK((e.max.y == Approx(2.0)));
+        CHECK(!e.isEmpty());
+        CHECK(e.min.x == Approx(0.0));
+        CHECK(e.min.y == Approx(0.0));
+        CHECK(e.max.x == Approx(2.0));
+        CHECK(e.max.y == Approx(2.0));
     }
 
     SECTION("check empty intersection") {
         aeExtent e = e1 & e2;
-        CHECK((e.isEmpty()));
-        CHECK((e.min.x == Approx(1.0)));
-        CHECK((e.min.y == Approx(1.0)));
-        CHECK((e.max.x == Approx(1.0)));
-        CHECK((e.max.y == Approx(1.0)));
+        CHECK(e.isEmpty());
+        CHECK(e.min.x == Approx(1.0));
+        CHECK(e.min.y == Approx(1.0));
+        CHECK(e.max.x == Approx(1.0));
+        CHECK(e.max.y == Approx(1.0));
     }
 
     SECTION("check non-empty intersection") {
         aeExtent e3 = { aePoint(0.5, 0.5), aePoint(1.5, 1.5) };
         aeExtent e = e1 & e3;
-        CHECK((!e.isEmpty()));
-        CHECK((e.min.x == Approx(0.5)));
-        CHECK((e.min.y == Approx(0.5)));
-        CHECK((e.max.x == Approx(1.0)));
-        CHECK((e.max.y == Approx(1.0)));
+        CHECK(!e.isEmpty());
+        CHECK(e.min.x == Approx(0.5));
+        CHECK(e.min.y == Approx(0.5));
+        CHECK(e.max.x == Approx(1.0));
+        CHECK(e.max.y == Approx(1.0));
     }
 }
 
